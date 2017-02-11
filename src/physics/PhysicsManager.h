066b08b6754a09ad184715118d574d80ba198059
kahosks <sksieu@cs.ucalgary.ca>
2017-02-11 13:19:58 -0700

    Replaced a "};"

--- src/physics/PhysicsManager.h
+++ src/physics/PhysicsManager.h
@@ -15,23 +15,23 @@ class PhysicsManager
 	PxProfileZoneManager* mProfileZoneManager;
 	PxTolerancesScale mTolerancesScale;
 	PxDefaultCpuDispatcher* mDispatcher;
-    PxVisualDebuggerConnection* gConnection = NULL;
+	PxVisualDebuggerConnection* gConnection = NULL;
 
-    bool mIsVehicleInAir = true;
+	bool mIsVehicleInAir = true;
 
 public:
 
-    PxScene* mScene;
-    PxPhysics* mPhysics;
-    PxCooking* mCooking;
-    PxMaterial* mMaterial;
-    VehicleSceneQueryData*	mVehicleSceneQueryData = NULL;
-    PxBatchQuery*			mBatchQuery = NULL;
-    PxVehicleDrivableSurfaceToTireFrictionPairs* mFrictionPairs = NULL;
+	PxScene* mScene;
+	PxPhysics* mPhysics;
+	PxCooking* mCooking;
+	PxMaterial* mMaterial;
+	VehicleSceneQueryData*	mVehicleSceneQueryData = NULL;
+	PxBatchQuery*			mBatchQuery = NULL;
+	PxVehicleDrivableSurfaceToTireFrictionPairs* mFrictionPairs = NULL;
 
 	PhysicsManager();
 	~PhysicsManager();
 	PxActor* PhysicsManager::createGroundPlane();
 	PxActor* PhysicsManager::createWallPlane(int, int, int, int, int);
-	PxRigidBody* PhysicsManager::createBlock(float,float,float);
+	PxRigidBody* PhysicsManager::createBlock(float, float, float);
 	void PhysicsManager::stepPhysics();
